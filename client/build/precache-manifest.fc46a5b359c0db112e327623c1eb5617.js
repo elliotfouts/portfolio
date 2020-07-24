7682f9eea48a87c13c034f55abd07dd3383c9bba
elliotfouts <elliotfouts@gmail.com>
2020-07-24 13:47:10 -0700

    production build v2

---
+++ client/build/precache-manifest.fc46a5b359c0db112e327623c1eb5617.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e953d3c105de0aeb11a1db123cd1275",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "68514a5374720e17e1ec",
+    "url": "/Portfolio/static/js/2.d128893f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Portfolio/static/js/2.d128893f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4ddada11cdbd9babec2",
+    "url": "/Portfolio/static/js/main.d78a847d.chunk.js"
+  },
+  {
+    "revision": "fd11c46505104759cf2b",
+    "url": "/Portfolio/static/js/runtime-main.b9da9406.js"
+  },
+  {
+    "revision": "732a490a5480b3fdfed2faa42672993b",
+    "url": "/Portfolio/static/media/Elliot_Fouts_Resume.732a490a.pdf"
+  },
+  {
+    "revision": "e765e3fef4d3242aa7ff3691fb28e7c2",
+    "url": "/Portfolio/static/media/code.e765e3fe.png"
+  },
+  {
+    "revision": "3b977052b4ac5641c8a036d02575e429",
+    "url": "/Portfolio/static/media/codifyDesktop.3b977052.png"
+  },
+  {
+    "revision": "f92339376a872037b664b9dffd89f814",
+    "url": "/Portfolio/static/media/codifyMobile.f9233937.png"
+  },
+  {
+    "revision": "057611a9ad4f045b21f37b0dd5862017",
+    "url": "/Portfolio/static/media/gitConnected.057611a9.png"
+  },
+  {
+    "revision": "a691dfaf4b5b4ba4612fdae5d99c4213",
+    "url": "/Portfolio/static/media/grocerease.a691dfaf.png"
+  },
+  {
+    "revision": "51c7abcc7264cdf3706f738abb19ab7b",
+    "url": "/Portfolio/static/media/javascriptQuiz.51c7abcc.png"
+  },
+  {
+    "revision": "441f6d27d4a44de4c936bf95287cf86e",
+    "url": "/Portfolio/static/media/weatherForecast.441f6d27.png"
+  }
+]);
