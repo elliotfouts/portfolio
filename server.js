686280d7b628e10040677f1f553b526f79bd75eb
elliotfouts <elliotfouts@gmail.com>
2020-07-24 11:04:33 -0700

    required path module for serving static assets

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const nodemailer = require('nodemailer');
+const path = require('path');
 const {nodemailerRoutes} = require('./routes');
 
 // instantiate express and set port
